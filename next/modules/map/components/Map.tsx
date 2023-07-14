8a34591961e79d4dc14c9db6be5beaba8822d932
swartzentrubern <swartzentrubern@si.edu>
2023-07-14 08:35:17 -0400

    Add example marker and popup
    Add the example marker and popup from the remaining parts of the leaflet
    setup example.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { MapContainer, TileLayer } from 'react-leaflet';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+} from 'react-leaflet';
 
 export default function Map() {
   return (
@@ -9,6 +14,14 @@ export default function Map() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup.
+          {' '}
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }
