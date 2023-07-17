d84b2289e02a8ad35a5a4fd8d17c4af44787e454
swartzentrubern <swartzentrubern@si.edu>
2023-07-17 08:18:39 -0400

    Set the initial map view
    Set the initial map view to be roughly around the east coast of the
    United States where the majority of the deployment site locations are.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[38.9533213, -77.0508073]} zoom={3} scrollWheelZoom>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
