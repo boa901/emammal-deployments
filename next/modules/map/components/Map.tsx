89c5df2be7723112d39c6c571ba791ed7cfdc22e
swartzentrubern <swartzentrubern@si.edu>
2023-07-17 14:25:31 -0400

    Remove default marker
    Remove the default marker from the react-leaflet guide.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -42,14 +42,6 @@ export default function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {markers}
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup.
-          {' '}
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 }
