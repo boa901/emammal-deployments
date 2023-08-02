79706a61da43f36040e3b4fbd0e02d5337cbe3a7
swartzentrubern <swartzentrubern@si.edu>
2023-08-02 13:52:13 -0400

    Add optional filter to getProjectDeployments
    Change the query speciees is one of the query parameters. This gets all
    deployments that have species sightings in the list of species
    specified. Return the deployment data for deployments meeting the
    criteria.

--- express/queries/getProjectDeployments.js
+++ express/queries/getProjectDeployments.js
@@ -1,7 +1,7 @@
 import pool from '#express/pool';
 
 const getProjectDeployments = async (req, res) => {
-  const query = `SELECT nid, label, latitude, longitude
+  let query = `SELECT nid, label, latitude, longitude
     FROM deployments_drupal
     WHERE sub_project IN (
       SELECT nid
@@ -9,10 +9,24 @@ const getProjectDeployments = async (req, res) => {
       WHERE project_nid = $1
     ) AND latitude IS NOT NULL AND longitude IS NOT NULL
     ORDER BY latitude`;
+  const values = [req.params.id];
+  if (req.query.species && req.query.species.length > 0) {
+    query = `SELECT DISTINCT a.nid, a.label, a.latitude, a.longitude
+    FROM deployments_drupal a
+      JOIN eda_deployments_drupal b ON a.nid = b.nid
+      JOIN eda_species_drupal c ON b.pid = c.pid AND c.species = ANY($2::varchar(255)[])
+    WHERE sub_project IN (
+      SELECT nid
+      FROM sub_projects_drupal
+      WHERE project_nid = $1
+    ) AND latitude IS NOT NULL AND longitude IS NOT NULL
+    ORDER BY a.nid`;
+    values.push(req.query.species);
+  }
   const results = await pool.query({
     rowMode: 'json',
     text: query,
-    values: [req.params.id],
+    values,
   });
   res.status(200).json(results.rows);
 };
