078ec5cafd907be62bff51ee6a7e344c15142cd4
swartzentrubern <swartzentrubern@si.edu>
2023-08-03 11:07:37 -0400

    Show species sighting counts for project species
    Add a sighting count to the express endpoint showing species summary for
    a given project. This can be used to illustrate a breakdown of species
    sightings for a given project on a project page.

--- express/queries/getProjectSpecies.js
+++ express/queries/getProjectSpecies.js
@@ -1,12 +1,13 @@
 import pool from '#express/pool';
 
 const getProjectSpecies = async (req, res) => {
-  const query = `SELECT DISTINCT a.species
+  const query = `SELECT a.species, SUM(a.count) count
     FROM eda_species_drupal a
       JOIN eda_deployments_drupal b ON a.pid = b.pid
       JOIN deployments_drupal c ON b.nid = c.nid
       JOIN sub_projects_drupal d ON c.sub_project = d.nid
     WHERE d.project_nid = $1
+    GROUP BY a.species
     `;
   const results = await pool.query({
     rowMode: 'json',
