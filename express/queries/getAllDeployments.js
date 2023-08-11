d37da88b71471b44896bb3a796b87519475f48b8
swartzentrubern <swartzentrubern@si.edu>
2023-08-11 15:17:27 -0400

    Add query parameters to deployments endpoint
    Add query parameters to the deployments endpoint. Add minLat, maxLat,
    minLng, maxLng to allow queries to filter by deployment location. This
    will be used to get deployments in a bounded box.

--- express/queries/getAllDeployments.js
+++ express/queries/getAllDeployments.js
@@ -1,10 +1,27 @@
 import pool from '#express/pool';
 
 const getAllDeployments = async (req, res) => {
-  const query = `SELECT nid, sub_project, label, latitude, longitude, camera_name
+  let query = `SELECT nid, sub_project, label, latitude, longitude, camera_name
     FROM deployments_drupal
     WHERE label IS NOT NULL AND latitude IS NOT NULL AND longitude IS NOT NULL;`;
-  const results = await pool.query(query);
+  let values = [];
+  if (req.query.maxLat && req.query.minLat && req.query.maxLng && req.query.minLng) {
+    query = `SELECT nid, sub_project, label, latitude, longitude, camera_name
+    FROM deployments_drupal
+    WHERE label IS NOT NULL AND latitude IS NOT NULL AND longitude IS NOT NULL
+      AND latitude BETWEEN $1 AND $2 AND longitude BETWEEN $3 AND $4;`;
+    values = [
+      req.query.minLat,
+      req.query.maxLat,
+      req.query.minLng,
+      req.query.maxLng,
+    ];
+  }
+  const results = await pool.query({
+    rowMode: 'json',
+    text: query,
+    values,
+  });
   res.status(200).json(results.rows);
 };
 
