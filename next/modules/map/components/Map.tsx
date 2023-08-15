fffe06d6551c4a7dafb0abe749bc3bfcd8bddfbb
swartzentrubern <swartzentrubern@si.edu>
2023-08-15 11:12:58 -0400

    Add loading animation to Map
    Add a loading animation to the Map component. This will show a loading
    spinner as projects or deployments are being added to the map as
    markers. This prevents an empty map from being displayed for an extended
    period of time as the user is waiting for markers to be loaded.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Spinner } from 'flowbite-react';
 import {
   useEffect,
   useState,
@@ -13,6 +14,7 @@ import Project from '@/common/types/project';
 import MapProps from '@/modules/map/types/MapProps';
 
 export default function Map({ apiPath, mapping }: MapProps) {
+  const [loading, setLoading] = useState<boolean>(true);
   const [markers, setMarkers] = useState<React.ReactNode | null>(null);
 
   useEffect(() => {
@@ -23,18 +25,27 @@ export default function Map({ apiPath, mapping }: MapProps) {
 
       const markerComponents: React.ReactNode = points.map(mapping);
       setMarkers(markerComponents);
+      setLoading(false);
     };
 
+    setLoading(true);
     fetchPoints();
   }, [apiPath]);
 
   return (
-    <MapContainer center={[38.9533213, -77.0508073]} zoom={3} scrollWheelZoom>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {markers}
-    </MapContainer>
+    <div className="container mx-auto my-2 relative">
+      <MapContainer center={[38.9533213, -77.0508073]} zoom={3} scrollWheelZoom>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markers}
+      </MapContainer>
+      {loading && (
+        <div className="absolute bg-gray-400 opacity-50 inset-0 flex justify-center items-center">
+          <Spinner size="xl" />
+        </div>
+      )}
+    </div>
   );
 }
