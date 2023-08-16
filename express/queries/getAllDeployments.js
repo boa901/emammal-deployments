f76aeada85872e745039b2f4f946f7547e9c53ef
swartzentrubern <swartzentrubern@si.edu>
2023-08-16 15:30:26 -0400

    Add multiple filters to express endpoint
    Allow getAllDeployments to apply a filter based on bounds and a list of
    species at the same time. This allows the different filters to be
    optionally applied based on which parameters are included.

--- express/queries/getAllDeployments.js
+++ express/queries/getAllDeployments.js
@@ -1,21 +1,23 @@
 import pool from '#express/pool';
 
 const getAllDeployments = async (req, res) => {
-  let query = `SELECT nid, sub_project, label, latitude, longitude, camera_name
-    FROM deployments_drupal
-    WHERE label IS NOT NULL AND latitude IS NOT NULL AND longitude IS NOT NULL;`;
-  let values = [];
+  let query = `SELECT a.nid, a.sub_project, a.label, a.latitude, a.longitude, a.camera_name
+    FROM deployments_drupal a`;
+  const values = [];
+  let paramCount = 1;
+  if (req.query.species && req.query.species.length > 0) {
+    query += `\nJOIN eda_deployments_drupal b ON a.nid = b.nid
+      JOIN eda_species_drupal c ON b.pid = c.pid AND c.species = ANY($${paramCount++}::varchar(255)[])`;
+    const species = JSON.parse(req.query.species);
+    values.push(species);
+  }
+  query += '\nWHERE label IS NOT NULL AND latitude IS NOT NULL AND longitude IS NOT NULL';
   if (req.query.maxLat && req.query.minLat && req.query.maxLng && req.query.minLng) {
-    query = `SELECT nid, sub_project, label, latitude, longitude, camera_name
-    FROM deployments_drupal
-    WHERE label IS NOT NULL AND latitude IS NOT NULL AND longitude IS NOT NULL
-      AND latitude BETWEEN $1 AND $2 AND longitude BETWEEN $3 AND $4;`;
-    values = [
-      req.query.minLat,
-      req.query.maxLat,
-      req.query.minLng,
-      req.query.maxLng,
-    ];
+    query += `\n  AND latitude BETWEEN $${paramCount++} AND $${paramCount++} AND longitude BETWEEN $${paramCount++} AND $${paramCount++}`;
+    values.push(req.query.minLat);
+    values.push(req.query.maxLat);
+    values.push(req.query.minLng);
+    values.push(req.query.maxLng);
   }
   const results = await pool.query({
     rowMode: 'json',
