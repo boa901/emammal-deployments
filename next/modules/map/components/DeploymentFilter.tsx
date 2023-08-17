9c0aecb44b0dd3bed02bb611ace0da3a6f2775ad
swartzentrubern <swartzentrubern@si.edu>
2023-08-17 09:26:29 -0400

    Handle species on submission
    When clicking the Search button, handle all filters that a user may
    select. This allows deployments to be filtered by location, species, or
    both.

--- next/modules/map/components/DeploymentFilter.tsx
+++ next/modules/map/components/DeploymentFilter.tsx
@@ -4,20 +4,27 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 import RectBounds from '@/modules/map/types/RectBounds';
-import GeoFilterMapBarrel from './GeoFilterMapBarrel';
+import GeoFilterMapBarrel from '@/modules/map/components/GeoFilterMapBarrel';
+import DeploymentSpeciesFilter from '@/modules/map/components/DeploymentSpeciesFilter';
 
 export default function DeploymentFilter() {
   const router = useRouter();
 
   const [rectBounds, setRectBounds] = useState<RectBounds>(null);
+  const [filterSpecies, setFilterSpecies] = useState<string[]>([]);
 
   const handleSubmit = () => {
-    router.push(`/deployments?${new URLSearchParams({ ...rectBounds })}`);
+    const params = {
+      species: JSON.stringify(filterSpecies),
+      ...rectBounds,
+    };
+    router.push(`/deployments?${new URLSearchParams(params).toString()}`);
   };
 
   return (
     <div>
       <GeoFilterMapBarrel setFilter={setRectBounds} />
+      <DeploymentSpeciesFilter setFilter={setFilterSpecies} />
       <button type="button" onClick={handleSubmit}>Search</button>
     </div>
   );
