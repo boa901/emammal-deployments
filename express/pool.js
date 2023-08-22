4353dd5cbb8c84a0824a487e6974b104571269ab
swartzentrubern <swartzentrubern@si.edu>
2023-08-22 09:41:25 -0400

    Create mariadb pool
    Change express/pool.js to start a mariadb pool instead of a postgres
    pool. Then express can query the mariadb instance through this
    connection.

--- express/pool.js
+++ express/pool.js
@@ -1,11 +1,21 @@
-import pg from 'pg';
+import mariadb from 'mariadb';
 import dotenv from 'dotenv';
-import { pgConnectionString } from '#express/settings';
+import {
+  mariadbHost,
+  mariadbPort,
+  mariadbUser,
+  mariadbPassword,
+  mariadbDatabase,
+} from '#express/settings';
 
 dotenv.config();
 
-const { Pool } = pg;
-const connectionString = encodeURI(pgConnectionString);
-const pool = new Pool({ connectionString });
+const pool = mariadb.createPool({
+  host: mariadbHost,
+  port: mariadbPort,
+  user: mariadbUser,
+  password: mariadbPassword,
+  database: mariadbDatabase,
+});
 
 export default pool;
