a66dcb679fa1e098a3efbe60f87f73c6d606bbbe
swartzentrubern <swartzentrubern@si.edu>
2023-08-24 12:40:03 -0400

    Update getAllProjects query
    Update the getAllProjects query to use MariaDB syntax and connector.

--- express/queries/getAllProjects.js
+++ express/queries/getAllProjects.js
@@ -2,9 +2,10 @@ import pool from '#express/pool';
 
 const getAllProjects = async (req, res) => {
   const query = `SELECT nid, name, latitude, longitude
-    FROM projects_drupal;`;
+    FROM projects
+    ORDER BY name`;
   const results = await pool.query(query);
-  res.status(200).json(results.rows);
+  res.status(200).json(results);
 };
 
 export default getAllProjects;
