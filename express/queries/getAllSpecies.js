d6936b09e4f7c328ba09250825a47a71c3988979
swartzentrubern <swartzentrubern@si.edu>
2023-08-24 12:40:41 -0400

    Update getAllSpecies query
    Update the getAllSpecies query to use MariaDB syntax and connector.

--- express/queries/getAllSpecies.js
+++ express/queries/getAllSpecies.js
@@ -2,9 +2,10 @@ import pool from '#express/pool';
 
 const getAllSpecies = async (req, res) => {
   const query = `SELECT DISTINCT species
-    FROM eda_species_drupal`;
+    FROM eda_species
+    ORDER BY species`;
   const results = await pool.query(query);
-  res.status(200).json(results.rows);
+  res.status(200).json(results);
 };
 
 export default getAllSpecies;
