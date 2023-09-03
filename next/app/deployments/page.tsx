980dedd444aa05dbb700d12b2bf62b83dee1e714
swartzentrubern <swartzentrubern@si.edu>
2023-09-03 11:01:55 -0400

    Add CSV download link to deployments page
    Add a CSV download link to the deployment page. Conditionally render the
    link if the data is finished being fetched. Add the csvData state
    variable to store the deployment metadata in a form that is usable with
    the react-csv CSVLink component.

--- next/app/deployments/page.tsx
+++ next/app/deployments/page.tsx
@@ -1,13 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+import { CSVLink } from 'react-csv';
+
 import MapBarrel from '@/modules/map/components/MapBarrel';
 import deploymentMapping from '@/modules/map/utils/deploymentMapping';
 
 export default function Page({ searchParams }: { searchParams }) {
+  const [csvData, setCsvData] = useState([]);
+
   return (
     <div className="container w-3/4 mx-auto flex flex-col justify-center items-center">
       <MapBarrel
         apiPath={`/api/deployments?${new URLSearchParams(searchParams).toString()}`}
         mapping={deploymentMapping}
+        setCsvData={setCsvData}
       />
+      {csvData && csvData.length > 0 && (
+        <CSVLink
+          data={csvData}
+        >
+          Download Deployment Data
+        </CSVLink>
+      )}
     </div>
   );
 }
