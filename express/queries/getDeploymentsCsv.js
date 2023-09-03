1c33ab7c217c01fc12c5fd3360377632392f08c3
swartzentrubern <swartzentrubern@si.edu>
2023-09-03 11:11:18 -0400

    Handle no deployments in getDeploymentsCsv query
    Handle the case where an empty array is passed in place of a list of
    deployment nids. This prevents the express app from crashing and
    throwing a SQL syntax error when binding the empty array to the query as
    a parameter value. Respond with empty array if no deployments are
    listed.

--- express/queries/getDeploymentsCsv.js
+++ express/queries/getDeploymentsCsv.js
@@ -2,34 +2,38 @@ import pool from '#express/pool';
 
 const getDeploymentsCsv = async (req, res) => {
   const deployments = req.body;
-  const query = `SELECT
-    deployment_id,
-    deployment_name,
-    actual_date_out,
-    retrieval_date,
-    actual_lat,
-    actual_long,
-    fuzzed,
-    project_id,
-    project_name,
-    sub_project_id,
-    sub_project_name,
-    plot_name,
-    plot_treatment,
-    camera_failure_details,
-    deployment_notes,
-    bait_type,
-    bait_description,
-    feature,
-    feature_type_methodology,
-    detection_distance,
-    sensitivity_setting,
-    camera_id
+  if (deployments.length > 0) {
+    const query = `SELECT
+      deployment_id,
+      deployment_name,
+      actual_date_out,
+      retrieval_date,
+      actual_lat,
+      actual_long,
+      fuzzed,
+      project_id,
+      project_name,
+      sub_project_id,
+      sub_project_name,
+      plot_name,
+      plot_treatment,
+      camera_failure_details,
+      deployment_notes,
+      bait_type,
+      bait_description,
+      feature,
+      feature_type_methodology,
+      detection_distance,
+      sensitivity_setting,
+      camera_id
     FROM deployments_metadata
     WHERE nid IN (?)`;
-  const values = [deployments];
-  const results = await pool.query(query, values);
-  res.status(200).json(results);
+    const values = [deployments];
+    const results = await pool.query(query, values);
+    res.status(200).json(results);
+  } else {
+    res.status(200).json([]);
+  }
 };
 
 export default getDeploymentsCsv;
