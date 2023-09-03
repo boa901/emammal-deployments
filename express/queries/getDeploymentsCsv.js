084e78fa9107909a261e3c63e5615546b329e574
swartzentrubern <swartzentrubern@si.edu>
2023-09-03 10:52:56 -0400

    List columns to select for CSV
    List the column names in the csv metadata query rather than selecting
    all columns. This prevents the node id from being included in the
    generated CSV.

--- express/queries/getDeploymentsCsv.js
+++ express/queries/getDeploymentsCsv.js
@@ -2,7 +2,29 @@ import pool from '#express/pool';
 
 const getDeploymentsCsv = async (req, res) => {
   const deployments = req.body;
-  const query = `SELECT *
+  const query = `SELECT
+    deployment_id,
+    deployment_name,
+    actual_date_out,
+    retrieval_date,
+    actual_lat,
+    actual_long,
+    fuzzed,
+    project_id,
+    project_name,
+    sub_project_id,
+    sub_project_name,
+    plot_name,
+    plot_treatment,
+    camera_failure_details,
+    deployment_notes,
+    bait_type,
+    bait_description,
+    feature,
+    feature_type_methodology,
+    detection_distance,
+    sensitivity_setting,
+    camera_id
     FROM deployments_metadata
     WHERE nid IN (?)`;
   const values = [deployments];
