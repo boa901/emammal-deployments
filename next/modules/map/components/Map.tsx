b914067993ffa80740392c03edfb117187f323e8
swartzentrubern <swartzentrubern@si.edu>
2023-09-03 11:00:03 -0400

    Add setCsvData prop to Map components
    Add the setCsvData prop to the Map and MapBarrel components. This allows
    a state setter to be specified in the component props, allowing CSV data
    to be set to a state variable. Add the setCsvData prop to the MapProps
    type.

--- next/modules/map/components/Map.tsx
+++ next/modules/map/components/Map.tsx
@@ -14,7 +14,7 @@ import MarkerClusterGroup from 'react-leaflet-cluster';
 import Project from '@/common/types/project';
 import MapProps from '@/modules/map/types/MapProps';
 
-export default function Map({ apiPath, mapping }: MapProps) {
+export default function Map({ apiPath, mapping, setCsvData }: MapProps) {
   const [loading, setLoading] = useState<boolean>(true);
   const [markers, setMarkers] = useState<React.ReactNode | null>(null);
 
@@ -27,6 +27,14 @@ export default function Map({ apiPath, mapping }: MapProps) {
       const markerComponents: React.ReactNode = points.map(mapping);
       setMarkers(markerComponents);
       setLoading(false);
+
+      if (setCsvData) {
+        const csvRows = await fetch('/api/deployments/csv', {
+          method: 'POST',
+          body: JSON.stringify(points.map((point) => point.nid)),
+        }).then((res) => res.json());
+        setCsvData(csvRows);
+      }
     };
 
     setLoading(true);
