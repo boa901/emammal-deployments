b05426d042bc2a1e2b5d3730a450d211d98b3361
swartzentrubern <swartzentrubern@si.edu>
2023-09-06 11:00:30 -0400

    Add location filter species and projects queries
    Handle latitude and longitude bounding parameters in the getAllSpecies
    and getAllProjects express queries, this will allow species and projects
    to be filtered based on what deployments are present within the latitude
    and longitude bounds.

--- express/queries/getAllProjects.js
+++ express/queries/getAllProjects.js
@@ -1,10 +1,19 @@
 import pool from '#express/pool';
 
 const getAllProjects = async (req, res) => {
-  const query = `SELECT nid, name, latitude, longitude
-    FROM projects
-    ORDER BY name`;
-  const results = await pool.query(query);
+  let query = `SELECT DISTINCT a.nid, a.name, a.latitude, a.longitude
+    FROM projects a`;
+  const values = [];
+  if (req.query.maxLat && req.query.minLat && req.query.maxLng && req.query.minLng) {
+    query += `\nJOIN sub_projects b ON a.nid = b.project_nid
+      JOIN deployments c ON b.nid = c.sub_project AND c.latitude BETWEEN ? AND ? AND c.longitude BETWEEN ? AND ?`;
+    values.push(req.query.minLat);
+    values.push(req.query.maxLat);
+    values.push(req.query.minLng);
+    values.push(req.query.maxLng);
+  }
+  query += '\nORDER BY a.name';
+  const results = await pool.query(query, values);
   res.status(200).json(results);
 };
 
--- express/queries/getAllSpecies.js
+++ express/queries/getAllSpecies.js
@@ -1,11 +1,20 @@
 import pool from '#express/pool';
 
 const getAllSpecies = async (req, res) => {
-  const query = `SELECT DISTINCT species
-    FROM eda_species
-    WHERE visible=1
-    ORDER BY species`;
-  const results = await pool.query(query);
+  let query = `SELECT DISTINCT a.species
+    FROM eda_species a`;
+  const values = [];
+  if (req.query.maxLat && req.query.minLat && req.query.maxLng && req.query.minLng) {
+    query += `\nJOIN eda_deployments b ON a.pid = b.pid
+      JOIN deployments c ON b.nid = c.nid AND c.latitude BETWEEN ? AND ? AND c.longitude BETWEEN ? AND ?`;
+    values.push(req.query.minLat);
+    values.push(req.query.maxLat);
+    values.push(req.query.minLng);
+    values.push(req.query.maxLng);
+  }
+  query += `\nWHERE a.visible=1
+    ORDER BY a.species`;
+  const results = await pool.query(query, values);
   res.status(200).json(results);
 };
 
