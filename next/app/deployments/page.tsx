a0f5e132f03fccb676304ae76ee6ac9802db566e
swartzentrubern <swartzentrubern@si.edu>
2023-09-13 09:33:35 -0400

    Use DeploymentFilter on deployments page
    On the /deployments page, show the DeploymentFilter component. This
    allows the user to filter deployments again after getting a prior result
    set.

--- next/app/deployments/page.tsx
+++ next/app/deployments/page.tsx
@@ -1,13 +1,7 @@
-import MapBarrel from '@/modules/map/components/MapBarrel';
-import deploymentMapping from '@/modules/map/utils/deploymentMapping';
+import DeploymentFilter from '@/modules/map/components/DeploymentFilter';
 
 export default function Page({ searchParams }: { searchParams }) {
   return (
-    <div className="container w-3/4 mx-auto flex flex-col justify-center items-center">
-      <MapBarrel
-        apiPath={`/api/deployments?${new URLSearchParams(searchParams).toString()}`}
-        mapping={deploymentMapping}
-      />
-    </div>
+    <DeploymentFilter apiPath={`/api/deployments?${new URLSearchParams(searchParams).toString()}`} />
   );
 }
