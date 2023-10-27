5ce26e3f5ebf7cc09aa4fdfc4f4ccbbc418fde89
swartzentrubern <swartzentrubern@si.edu>
2023-10-27 10:27:39 -0400

    Use dynamic imports on project pages
    Use dynamic imports on the Filter and MapBarrel component on the project
    pages. This prevents client-side code from being evaluated on the
    server-side.

--- next/app/projects/[projectId]/page.tsx
+++ next/app/projects/[projectId]/page.tsx
@@ -1,6 +1,9 @@
-import Filter from '@/modules/map/components/Filter';
-import MapBarrel from '@/modules/map/components/MapBarrel';
+import dynamic from 'next/dynamic';
+
 import deploymentMapping from '@/modules/map/utils/deploymentMapping';
+
+const Filter = dynamic(() => import('@/modules/map/components/Filter'), { ssr: false });
+const MapBarrel = dynamic(() => import('@/modules/map/components/MapBarrel'), { ssr: false });
 
 export default function Page(
   { params, searchParams }: { params: { projectId: string }, searchParams },
