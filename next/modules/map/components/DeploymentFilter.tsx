1701a248547d76b367780978a25fff82858f3162
swartzentrubern <swartzentrubern@si.edu>
2023-10-27 09:52:33 -0400

    Use dynamic imports in DeploymentFilter
    Use dynamic imports in the DeploymentFilter component on the map and
    multiselect components, which should operate only in the client. This
    prevents references to window, document, or other client-side only code
    from being evaluated on the server.

--- next/modules/map/components/DeploymentFilter.tsx
+++ next/modules/map/components/DeploymentFilter.tsx
@@ -3,12 +3,14 @@
 import { Button } from 'flowbite-react';
 import { CSVLink } from 'react-csv';
 import { useRouter } from 'next/navigation';
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 
 import RectBounds from '@/modules/map/types/RectBounds';
-import GeoFilterMapBarrel from '@/modules/map/components/GeoFilterMapBarrel';
-import DeploymentMultiselect from '@/modules/map/components/DeploymentMultiselect';
 import deploymentMapping from '@/modules/map/utils/deploymentMapping';
+
+const GeoFilterMapBarrel = dynamic(() => import('@/modules/map/components/GeoFilterMapBarrel'), { ssr: false });
+const DeploymentMultiselect = dynamic(() => import('@/modules/map/components/DeploymentMultiselect'), { ssr: false });
 
 export default function DeploymentFilter({
   apiPath = null,
