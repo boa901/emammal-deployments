78f675355fc0edde1ac2bd82f55ddfb5d0593b84
swartzentrubern <swartzentrubern@si.edu>
2023-10-30 10:57:19 -0400

    Add next eslint to application
    Add the next eslint config to the project. Make the necessary changes to
    comply with the lint rules by importing the leaflet and leaflet draw css
    rather than linking with html elements in the layout. Add leaflet-draw
    as an explicit dependency.

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,22 +1,12 @@
 import './globals.css';
 import './leaflet.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-          crossOrigin=""
-        />
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css"
-          crossOrigin=""
-        />
-      </head>
+      <head />
       <body>{children}</body>
     </html>
   );
