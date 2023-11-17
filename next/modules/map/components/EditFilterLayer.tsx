67bf28f5db3f5a1d4bb39dd355377b335f263f2a
swartzentrubern <swartzentrubern@si.edu>
2023-11-17 15:50:18 -0500

    Fix delete functionality
    Fix an issue where the delete button does not delete the drawn
    rectangle. Set the layer to null to prevent a rectangle from being shown
    as EditFilterLayer remounts.

--- next/modules/map/components/EditFilterLayer.tsx
+++ next/modules/map/components/EditFilterLayer.tsx
@@ -38,6 +38,7 @@ export default function EditFilterLayer({ setFilter, layer, setLayer }) {
       }}
       onDeleted={() => {
         setFilter(null);
+        setLayer(null);
       }}
     />
   );
