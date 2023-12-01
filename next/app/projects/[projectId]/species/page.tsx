35e70e58f34bd20165c547a6faad30de8be034d3
swartzentrubern <swartzentrubern@si.edu>
2023-12-01 10:59:24 -0500

    Add loading animation layout to species table page
    On the species table page, show a grayed out table and a spinner as data
    is loaded into the table.

--- next/app/projects/[projectId]/species/page.tsx
+++ next/app/projects/[projectId]/species/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Table } from 'flowbite-react';
+import { Spinner, Table } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 
 export default function Page({ params }: { params: { projectId: string } }) {
@@ -8,7 +8,7 @@ export default function Page({ params }: { params: { projectId: string } }) {
     species: string,
     name: string,
     count: number,
-  }[]>([]);
+  }[] | null>(null);
 
   useEffect(() => {
     const fetchSpecies = async () => {
@@ -23,25 +23,28 @@ export default function Page({ params }: { params: { projectId: string } }) {
   }, []);
 
   return (
-    <div className="container w-3/4 mx-auto flex flex-col justify-center items-center">
-      <div className="w-full">
-        <Table hoverable>
-          <Table.Head>
-            <Table.HeadCell>Species</Table.HeadCell>
-            <Table.HeadCell>Scientific Name</Table.HeadCell>
-            <Table.HeadCell>Count</Table.HeadCell>
-          </Table.Head>
-          <Table.Body>
-            {data?.map((row) => (
-              <Table.Row key={row.species}>
-                <Table.Cell>{row.name}</Table.Cell>
-                <Table.Cell>{row.species}</Table.Cell>
-                <Table.Cell>{row.count}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </div>
+    <>
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Species</Table.HeadCell>
+          <Table.HeadCell>Scientific Name</Table.HeadCell>
+          <Table.HeadCell>Count</Table.HeadCell>
+        </Table.Head>
+        <Table.Body>
+          {data?.map((row) => (
+            <Table.Row key={row.species}>
+              <Table.Cell>{row.name}</Table.Cell>
+              <Table.Cell>{row.species}</Table.Cell>
+              <Table.Cell>{row.count}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+      {!data && (
+        <div className="w-full aspect-[2/1] grid place-content-center bg-gray-200">
+          <Spinner size="xl" />
+        </div>
+      )}
+    </>
   );
 }
