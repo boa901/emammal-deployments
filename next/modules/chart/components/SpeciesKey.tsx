e01a35a82a311a954e2667b376879c65691fe794
swartzentrubern <swartzentrubern@si.edu>
2023-12-04 14:04:28 -0500

    Create SpeciesKey component
    Create a new component in the chart module called SpeciesKey. This
    creates a key given the chart segment data.

---
+++ next/modules/chart/components/SpeciesKey.tsx
@@ -0,0 +1,16 @@
+export default function SpeciesKey({ data }: { data: {
+  species: string,
+  value: number,
+  color: string,
+}[] }) {
+  return (
+    <div className="flex flex-col justify-center">
+      {data.map((segment) => (
+        <div className="w-full flex flex-row items-center mx-4 my-1" key={segment.color}>
+          <div className="w-8 h-4 border-2 border-white mr-2" style={{ backgroundColor: segment.color }} />
+          <div className="">{segment.species}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
