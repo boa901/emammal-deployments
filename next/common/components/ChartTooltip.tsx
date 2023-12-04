cc9cc2b16bc9379f34f55509ed76ea0dc98612a9
swartzentrubern <swartzentrubern@si.edu>
2023-12-04 11:04:50 -0500

    Create ChartTooltip component
    Create a ChartTooltip component. This component is a custom
    implementation of a tooltip that follows the user's mouse. Include the
    text passed in content in the tooltip.

---
+++ next/common/components/ChartTooltip.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function ChartTooltip({ content }: { content: string }) {
+  const [mousePosition, setMousePosition] = useState<{ x: number, y: number }>();
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => setMousePosition({ x: e.clientX, y: e.clientY }));
+
+    return () => window.removeEventListener('mousemove', (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    });
+  }, []);
+
+  return mousePosition && content && content.length > 0 ? (
+    <div
+      style={{
+        position: 'absolute',
+        left: mousePosition.x + 15,
+        top: mousePosition.y,
+      }}
+      className="flex flex-row content-center bg-gray-900 text-white rounded px-2"
+    >
+      {content}
+    </div>
+  ) : null;
+}
