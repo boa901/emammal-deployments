5a0151014e250b606fe13288137e5b7b7da1c42e
swartzentrubern <swartzentrubern@si.edu>
2023-12-04 09:10:37 -0500

    Add /project/[project_id]/chart page
    Add a chart page for each project. Represent the 10 most sighted species
    in a pie chart. While the species data is loading, show a spinner.
    Design the chart to reflect the chart in the old emammal site as closely
    as possible.

---
+++ next/app/projects/[projectId]/chart/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Spinner } from 'flowbite-react';
+import { PieChart } from 'react-minimal-pie-chart';
+import { useEffect, useState } from 'react';
+
+export default function Page({ params }: { params: { projectId: string } }) {
+  const [data, setData] = useState<{
+    title: string,
+    value: number,
+    color: string,
+  }[] | null>(null);
+
+  const colors = [
+    '#FFCD56',
+    '#CFD8AD',
+    '#DDBBAA',
+    '#779922',
+    '#A87600',
+    '#D9F279',
+    '#775555',
+    '#883311',
+    '#664700',
+    '#4C473D',
+  ];
+
+  useEffect(() => {
+    const fetchSpecies = async () => {
+      const species = await fetch(`/api/projects/${params.projectId}/species`, {
+        method: 'GET',
+      }).then((res) => res.json()).then((results) => results.map((result) => (
+        { name: result.name, count: JSON.parse(result.count), species: result.species }
+      )));
+
+      const topSpecies = species.sort((a, b) => b.count - a.count).slice(0, 10);
+      setData(topSpecies.map((sighting, i) => ({
+        title: sighting.name,
+        value: sighting.count,
+        color: colors[i],
+      })));
+    };
+
+    fetchSpecies();
+  }, []);
+
+  return data ? (
+    <div className="w-full aspect-square">
+      <PieChart
+        data={data}
+        startAngle={-90}
+      />
+    </div>
+  ) : (
+    <div className="w-full aspect-square flex justify-center items-center">
+      <Spinner size="xl" />
+    </div>
+  );
+}
