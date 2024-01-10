e2b9cb36c391f3d9e3a9cdad8a67cc8e91c5561b
Noah Swartzentruber <swartzentrubern@si.edu>
2024-01-10 10:50:35 -0500

    Change close button to an X icon
    Change the close button to an X icon. It takes less space and is more
    visibly pleasing than a spelled out word on the button.

--- next/modules/map/components/DeploymentDrawer.tsx
+++ next/modules/map/components/DeploymentDrawer.tsx
@@ -33,12 +33,17 @@ export default function DeploymentDrawer({ isOpen, setOpen, deployment }) {
           <div className="flex-grow flex justify-center items-center">
             <h3>Deployment</h3>
           </div>
-          <Button onClick={(e) => {
-            setOpen(false);
-            e.currentTarget.blur();
-          }}
+          <Button
+            color="light"
+            outline={false}
+            onClick={(e) => {
+              setOpen(false);
+              e.currentTarget.blur();
+            }}
           >
-            Close
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
           </Button>
         </div>
         <Table hoverable>
