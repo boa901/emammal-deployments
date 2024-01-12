69d6df9fb71ba34a9743336dd06494fd950138ae
Noah Swartzentruber <swartzentrubern@si.edu>
2024-01-12 14:49:11 -0500

    Update getDeployment query
    Update the express getDeployment query to reflect the current live
    query. This is necessary in order to use express to test before making
    changes on the drupal instance remotely.

--- express/queries/getDeployment.js
+++ express/queries/getDeployment.js
@@ -1,8 +1,9 @@
 import pool from '#express/pool';
 
 const getDeployment = async (req, res) => {
-  const query = `SELECT s.species, s.count, a.nid
+  const query = `SELECT s.species, cn.name, s.count, a.nid
     FROM eda_species s
+      LEFT JOIN common_name cn on s.species = cn.species
       JOIN (
         SELECT pid, nid
         FROM eda_deployments
