af117cb40745f714fc240899efbb35efb0a6b1f0
Noah Swartzentruber <swartzentrubern@si.edu>
2024-01-16 11:15:46 -0500

    Add project name to drawer title
    Add the project name to the deployment drawer title. Change conditional
    rendering to render only the message for no deployment selected, or the
    title, table, and content when projectData and deployment are both
    defined.

--- next/modules/map/components/DeploymentDrawer.tsx
+++ next/modules/map/components/DeploymentDrawer.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { Table } from 'flowbite-react';
+import { Spinner, Table } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 
 export default function DeploymentDrawer({ isOpen, deployment }) {
-  const [data, setData] = useState<{
+  const [speciesData, setSpeciesData] = useState<{
     species: string,
     name: string,
     count: number,
   }[] | null>();
+  const [projectData, setProjectData] = useState<{
+    name: string,
+    nid: number,
+  } | null>();
 
   useEffect(() => {
     if (deployment) {
@@ -17,64 +21,78 @@ export default function DeploymentDrawer({ isOpen, deployment }) {
           method: 'GET',
         }).then((res) => res.json());
 
-        setData(deploymentData.species);
+        setSpeciesData(deploymentData.species);
+        setProjectData(deploymentData.project[0]);
       };
 
       fetchDeployment();
     } else {
-      setData(null);
+      setSpeciesData(null);
+      setProjectData(null);
     }
   }, [deployment]);
 
   return (
     <div className={`h-full transition-all duration-500 overflow-x-hidden ${isOpen ? 'w-1/5 mr-2' : 'w-0 pointer-events-none'}`}>
-      <div className="flex flex-col">
-        <div className="flex flex-row">
-          <div className="flex-grow flex justify-center items-center">
-            {deployment ? (
-              <h3 className="my-2 font-semibold">{deployment.label}</h3>
-            ) : (
-              <h3 className="my-2 font-semibold">Select Deployment...</h3>
-            )}
-          </div>
-        </div>
-        <Table hoverable>
-          <Table.Head>
-            <Table.HeadCell className="w-1/2">Species</Table.HeadCell>
-            <Table.HeadCell className="w-1/2">Count</Table.HeadCell>
-          </Table.Head>
-          <Table.Body className="overflow-y-scroll">
-            {data ? (
-              <>
-                {data.length > 0 ? (
-                  data.map((row) => (
-                    <Table.Row key={row.species}>
-                      <Table.Cell className="w-1/2">{row.name}</Table.Cell>
-                      <Table.Cell className="w-1/2">{row.count}</Table.Cell>
+      {deployment ? (
+        <>
+          {projectData ? (
+            <div className="flex flex-col">
+              <div className="flex flex-row">
+                <div className="flex-grow flex flex-wrap justify-center items-center">
+                  <h3 className="my-2 font-semibold text-center">
+                    {`${projectData.name}: ${deployment.label}`}
+                  </h3>
+                </div>
+              </div>
+              <Table hoverable>
+                <Table.Head>
+                  <Table.HeadCell className="w-1/2">Species</Table.HeadCell>
+                  <Table.HeadCell className="w-1/2">Count</Table.HeadCell>
+                </Table.Head>
+                <Table.Body className="overflow-y-scroll">
+                  {speciesData ? (
+                    <>
+                      {speciesData.length > 0 ? (
+                        speciesData.map((row) => (
+                          <Table.Row key={row.species}>
+                            <Table.Cell className="w-1/2">{row.name}</Table.Cell>
+                            <Table.Cell className="w-1/2">{row.count}</Table.Cell>
+                          </Table.Row>
+                        ))
+                      ) : (
+                        <Table.Row>
+                          <Table.Cell colSpan={2}>
+                            <div className="grid justify-items-center">
+                              <div>No species data for the current selection</div>
+                            </div>
+                          </Table.Cell>
+                        </Table.Row>
+                      )}
+                    </>
+                  ) : (
+                    <Table.Row>
+                      <Table.Cell colSpan={2}>
+                        <div className="grid justify-items-center">
+                          <Spinner />
+                        </div>
+                      </Table.Cell>
                     </Table.Row>
-                  ))
-                ) : (
-                  <Table.Row>
-                    <Table.Cell colSpan={2}>
-                      <div className="grid justify-items-center">
-                        <div>No species data for the current selection</div>
-                      </div>
-                    </Table.Cell>
-                  </Table.Row>
-                )}
-              </>
-            ) : (
-              <Table.Row>
-                <Table.Cell colSpan={2}>
-                  <div className="grid justify-items-center">
-                    <div>No deployment selected</div>
-                  </div>
-                </Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
-      </div>
+                  )}
+                </Table.Body>
+              </Table>
+            </div>
+          ) : (
+            <div className="h-full grid justify-items-center items-center">
+              <Spinner size="xl" />
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="h-full grid justify-items-center items-center">
+          <div>No deployment selected</div>
+        </div>
+      )}
     </div>
   );
 }
