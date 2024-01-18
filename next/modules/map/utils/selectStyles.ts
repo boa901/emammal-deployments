332a73c0f4e4a8a8c3d120364ac58204972464e3
Noah Swartzentruber <swartzentrubern@si.edu>
2024-01-18 10:57:18 -0500

    Set multiselect to allow scrolling overflow
    Set the maxHeights of the child elements of the multiselect to 36px to
    match the container size and prevent unnecessary scroll bars. When
    selecting long-named or many values, the user can scroll to read all the
    selected options. This also prevents the selected values from causing
    the multiselect not to shrink when resizing.

--- next/modules/map/utils/selectStyles.ts
+++ next/modules/map/utils/selectStyles.ts
@@ -5,15 +5,20 @@ const selectStyles = {
   }),
   control: (base) => ({
     ...base,
-    maxHeight: '35px',
+    maxHeight: '36px',
   }),
   valueContainer: (base) => ({
     ...base,
-    maxHeight: '35px',
+    maxHeight: '36px',
+    overflow: 'auto',
+  }),
+  multiValueLabel: (base) => ({
+    ...base,
+    whiteSpace: 'normal',
   }),
   indicatorsContainer: (base) => ({
     ...base,
-    maxHeight: '35px',
+    maxHeight: '36px',
   }),
 };
 
