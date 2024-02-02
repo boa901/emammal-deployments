ad183eae45975b41ad96f5531e2e6e64cab68cc2
Noah Swartzentruber <swartzentrubern@si.edu>
2024-02-02 11:12:15 -0500

    Filters and buttons flex direction breakpoint
    Change the multiselects div and buttons div flex direction when reaching
    the lg breakpoint from col to row. This utilizes more of the horizontal
    space given.

--- next/modules/map/components/DeploymentFilter.tsx
+++ next/modules/map/components/DeploymentFilter.tsx
@@ -80,8 +80,8 @@ export default function DeploymentFilter({
   return (
     <div className="h-screen flex flex-col">
       {mapReady && (
-        <div className="w-full flex flex-col my-2">
-          <div className="flex flex-col sm:flex-row">
+        <div className="w-full flex flex-col my-2 lg:flex-row">
+          <div className="grow flex flex-col sm:flex-row">
             <div className="p-2 sm:w-1/2">
               <DeploymentMultiselect
                 setFilter={setFilterSpecies}
