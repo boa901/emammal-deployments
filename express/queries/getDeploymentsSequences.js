887762f0f2097793796f89b10e18e2a4ac3df4ae
Noah Swartzentruber <swartzentrubern@si.edu>
2024-02-12 11:41:43 -0500

    Add sequence query
    Add a query to the express server to query sequence data based on a
    collection of deployment nids.

---
+++ express/queries/getDeploymentsSequences.js
@@ -0,0 +1,44 @@
+import pool from '#express/pool';
+
+const getDeploymentsSequences = async (req, res) => {
+  const deployments = req.body;
+  if (deployments.length > 0) {
+    const query = `SELECT
+      s.project_id,
+      s.deployment_id,
+      s.sequence_id,
+      s.is_blank,
+      s.identified_by,
+      s.wi_taxon_id,
+      s.class,
+      s.order,
+      s.family,
+      s.genus,
+      s.species,
+      s.common_name,
+      s.uncertainty,
+      s.start_time,
+      s.end_time,
+      s.group_size,
+      s.age,
+      s.sex,
+      s.animal_recognizable,
+      s.individual_id,
+      s.individual_animal_notes,
+      s.behavior,
+      s.highlighted,
+      s.markings,
+      s.cv_confidence,
+      s.license
+    FROM sidora_sequences s
+    LEFT JOIN eda_deployments d on s.deployment_id = concat(d.ctLabel, '_', d.cameraCiteinfo)
+    WHERE d.nid IN (?)`;
+    const values = [deployments];
+    const results = await pool.query(query, values);
+    res.status(200).json(results);
+  } else {
+    res.status(200).json([]);
+  }
+};
+
+export default getDeploymentsSequences;
