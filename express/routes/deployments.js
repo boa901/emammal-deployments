443cdf9f87693f1c3c265be54fbf0e76401bd3c2
Noah Swartzentruber <swartzentrubern@si.edu>
2024-02-12 11:43:25 -0500

    Add sequence endpoint
    Add an endpoint that uses the sequence data query. The endpoint can be
    accessed at /deployments/sequences.

--- express/routes/deployments.js
+++ express/routes/deployments.js
@@ -2,12 +2,14 @@ import express from 'express';
 
 import getDeployment from '#express/queries/getDeployment';
 import getDeploymentsCsv from '#express/queries/getDeploymentsCsv';
+import getDeploymentsSequences from '#express/queries/getDeploymentsSequences';
 import getAllDeployments from '#express/queries/getAllDeployments';
 
 const router = express.Router();
 
 router.get('/:id', getDeployment);
 router.post('/csv', getDeploymentsCsv);
+router.post('/sequences', getDeploymentsSequences);
 router.get('/', getAllDeployments);
 
 export default router;
