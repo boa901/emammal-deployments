96f8370e916bb55241022f551e4050a06be97984
Noah Swartzentruber <swartzentrubern@si.edu>
2024-02-13 12:13:23 -0500

    Handle no sequence data
    Handle the case where there is no sequence data for the currently
    selected set of deployments. This prevents a user from attempting to
    download no data.

--- next/modules/sequence/components/SequenceDownload.tsx
+++ next/modules/sequence/components/SequenceDownload.tsx
@@ -4,7 +4,13 @@ import { Spinner } from 'flowbite-react';
 
 export default function SequenceDownload({ sequenceData }): React.ReactNode {
   return sequenceData ? (
-    <>{`You are about to download ${sequenceData.length} sequence data records. Are you sure you would like to proceed?`}</>
+    <>
+      {sequenceData.length > 0 ? (
+        <>{`You are about to download ${sequenceData.length} sequence data records. Are you sure you would like to proceed?`}</>
+      ) : (
+        <>There are no sequences for this set of deployments, please adjust your selection.</>
+      )}
+    </>
   ) : (
     <div className="flex w-full h-full content-center justify-center">
       <Spinner />
