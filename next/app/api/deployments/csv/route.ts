eac191133be2c5177a358f4dc9657d25ce199b8b
Noah Swartzentruber <swartzentrubern@si.edu>
2024-02-21 15:48:46 -0500

    Set csv column order
    In the /api/deployments/csv API endpoint, set the order of the columns
    by mapping the response from the backend. This ensures that the order of
    the csv columns will be determined regardless of their order in the
    response from the backend.

--- next/app/api/deployments/csv/route.ts
+++ next/app/api/deployments/csv/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const deployments = await request.json();
-  const points = await fetch(
+  const metadataRows = await fetch(
     `${process.env.API_DOMAIN}/deployments/csv`, {
       method: 'POST',
       headers: {
@@ -13,5 +13,28 @@ export async function POST(request: Request) {
     },
   ).then((res) => res.json());
 
-  return NextResponse.json(points);
+  return NextResponse.json(metadataRows.map((metadata) => ({
+    deployment_id: metadata.deployment_id,
+    deployment_name: metadata.deployment_name,
+    actual_date_out: metadata.actual_date_out,
+    retrieval_date: metadata.retrieval_date,
+    actual_lat: metadata.actual_lat,
+    actual_long: metadata.actual_long,
+    fuzzed: metadata.fuzzed,
+    project_id: metadata.project_id,
+    project_name: metadata.project_name,
+    sub_project_id: metadata.sub_project_id,
+    sub_project_name: metadata.sub_project_name,
+    plot_name: metadata.plot_name,
+    plot_treatment: metadata.plot_treatment,
+    camera_failure_details: metadata.camera_failure_details,
+    deployment_notes: metadata.deployment_notes,
+    bait_type: metadata.bait_type,
+    bait_description: metadata.bait_description,
+    feature: metadata.feature,
+    feature_type_methodology: metadata.feature_type_methodology,
+    detection_distance: metadata.detection_distance,
+    sensitivity_setting: metadata.sensitivity_setting,
+    camera_id: metadata.camera_id,
+  })));
 }
