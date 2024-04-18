de6c4f53d64a6abda300888c8c58f21563b9ef93
Noah Swartzentruber <swartzentrubern@si.edu>
2024-04-18 11:07:20 -0400

    Use skeleton loading indicator in the modal
    When the data export modal is loading, show a skeleton screen pulsing
    loading indicator to show that the files are being generated for
    download.

--- next/modules/export/components/DeploymentDownload.tsx
+++ next/modules/export/components/DeploymentDownload.tsx
@@ -1,34 +1,35 @@
 'use client';
 
-import {
-  Spinner,
-  Tooltip,
-} from 'flowbite-react';
+import { Tooltip } from 'flowbite-react';
 import { CSVLink } from 'react-csv';
 
 export default function DeploymentDownload({ deploymentData, fileName }): React.ReactNode {
-  return deploymentData ? (
-    <div>
-      {deploymentData.length > 0 ? (
-        <CSVLink
-          data={deploymentData}
-          filename={fileName}
-        >
-          <button type="button" className="text-cyan-700 hover:underline">
-            deployment_metadata.csv
-          </button>
-        </CSVLink>
+  return (
+    <div className="flex flex-col justify-center mb-2 h-4">
+      {deploymentData ? (
+        <div>
+          {deploymentData.length > 0 ? (
+            <CSVLink
+              data={deploymentData}
+              filename={fileName}
+            >
+              <button type="button" className="text-cyan-700 hover:underline">
+                deployment_metadata.csv
+              </button>
+            </CSVLink>
+          ) : (
+            <Tooltip content="No deployment data available">
+              <button type="button" className="text-cyan-700 opacity-50 select-none">
+                deployment_metadata.csv
+              </button>
+            </Tooltip>
+          )}
+        </div>
       ) : (
-        <Tooltip content="No deployment data available">
-          <button type="button" className="text-cyan-700 opacity-50 select-none">
-            deployment_metadata.csv
-          </button>
-        </Tooltip>
+        <div role="status" className="animate-pulse h-full w-full">
+          <div className="h-full bg-gray-200 rounded-full dark:bg-gray-700 w-full" />
+        </div>
       )}
-    </div>
-  ) : (
-    <div className="flex w-full content-center justify-center">
-      <Spinner />
     </div>
   );
 }
--- next/modules/export/components/SequenceDownload.tsx
+++ next/modules/export/components/SequenceDownload.tsx
@@ -1,42 +1,43 @@
 'use client';
 
-import {
-  Spinner,
-  Tooltip,
-} from 'flowbite-react';
+import { Tooltip } from 'flowbite-react';
 import { CSVLink } from 'react-csv';
 
 export default function SequenceDownload({ sequenceData, modalError, fileName }): React.ReactNode {
-  return sequenceData ? (
-    <div>
-      {sequenceData.length > 0 ? (
-        <CSVLink
-          data={sequenceData}
-          filename={fileName}
-        >
-          <button type="button" className="text-cyan-700 hover:underline">
-            sequence_data.csv
-          </button>
-        </CSVLink>
+  return (
+    <div className="flex flex-col justify-center mb-2 h-4">
+      {sequenceData ? (
+        <div>
+          {sequenceData.length > 0 ? (
+            <CSVLink
+              data={sequenceData}
+              filename={fileName}
+            >
+              <button type="button" className="text-cyan-700 hover:underline">
+                sequence_data.csv
+              </button>
+            </CSVLink>
+          ) : (
+            <Tooltip content="No sequences for selected deployments.">
+              <button type="button" className="text-cyan-700 opacity-50 select-none">
+                sequence_data.csv
+              </button>
+            </Tooltip>
+          )}
+        </div>
       ) : (
-        <Tooltip content="No sequences for selected deployments.">
-          <button type="button" className="text-cyan-700 opacity-50 select-none">
-            sequence_data.csv
-          </button>
-        </Tooltip>
-      )}
-    </div>
-  ) : (
-    <div>
-      {modalError ? (
-        <Tooltip content={modalError}>
-          <button type="button" className="text-cyan-700 opacity-50 select-none">
-            sequence_data.csv
-          </button>
-        </Tooltip>
-      ) : (
-        <div className="flex w-full content-center justify-center">
-          <Spinner />
+        <div className="h-full w-full">
+          {modalError ? (
+            <Tooltip content={modalError}>
+              <button type="button" className="text-cyan-700 opacity-50 select-none">
+                sequence_data.csv
+              </button>
+            </Tooltip>
+          ) : (
+            <div role="status" className="animate-pulse h-full w-full">
+              <div className="h-full bg-gray-200 rounded-full dark:bg-gray-700 w-full" />
+            </div>
+          )}
         </div>
       )}
     </div>
